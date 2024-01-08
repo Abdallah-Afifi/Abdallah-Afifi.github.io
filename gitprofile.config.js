686535c41c6ad45e2941b832d2349307ccb25f77
Abdallah Afifi <abdallahfathi@aucegypt.edu>
2024-01-08 21:37:35 +0200

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -112,7 +112,7 @@ const config = {
 
     // Should use the prefers-color-scheme media-query,
     // using user system preferences, instead of the hardcoded defaultTheme
-    respectPrefersColorScheme: true,
+    respectPrefersColorScheme: false,
 
     // Hide the ring in Profile picture
     hideAvatarRing: false,
